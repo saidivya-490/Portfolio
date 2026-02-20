038cc585f788eabd0dcc472aa9b8ec07f7f2521c
saidivya-490 <sai.divya490@gmail.com>
2026-02-20 09:45:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const LINKS = {
   resumePdf: "resume.pdf", // upload resume.pdf into repo root
   linkedin: "https://www.linkedin.com/in/sai-k-853685143/",
-  email: "Saidivya.490@gmail.com"
+  email: "Sai.divya490@gmail.com"
 };
 
 document.getElementById("year").textContent = new Date().getFullYear();
